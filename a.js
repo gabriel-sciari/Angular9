68e359bdd6a829c8c9963b5329b2b3ec66284a2c
annakarpf <anakarpf6@gmail.com>
2020-03-05 14:00:17 +0200

    angular

--- a.js
+++
@@ -1,9 +0,0 @@
-let arr=[{"name":"Afghanistan","population":27657145},
-{"name":"Åland Islands","population":28875},
-{"name":"Israel","population":70000000000},
-{"name":"UK","population":30000}];
-
-let res=
-
-
-console.log(res);
